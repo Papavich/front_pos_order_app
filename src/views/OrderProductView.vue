b4d912c39fa18642b0e7829e355da3aee32682cc
Papavich Pasri <33253796+Papavich@users.noreply.github.com>
2024-04-17 15:13:52 +0700

    frontend update for order page

--- src/views/OrderProductView.vue
+++ src/views/OrderProductView.vue
@@ -1,52 +1,77 @@
 <template>
-  <div class="create-order-container">
-    <!-- Order Product -->
-    <!-- show product id by params-->
-    <!-- {{ $route.params.productId }} -->
-    <div class="create-order__left">
-      <div class="create-order__product-detail">
-        <v-card class="create-order__product-detail-card">
-          <div class="order-product__img">
-            <img :src="productById.productURL" alt="" width="300" />
+  <div>
+    <NavbarComponent />
+    <!-- content -->
+    <div class="create-order-container">
+      <!-- Order Product -->
+      <!-- show product id by params-->
+      <!-- {{ $route.params.productId }} -->
+      <div class="create-order__left">
+        <div class="create-order__product-detail">
+          <div class="create-order__product-detail-card">
+            <div class="order-product__img">
+              <img :src="productById.productURL" alt="" width="300" />
+            </div>
+            <div class="order-product__name">
+              <h1>{{ productById.productName }}</h1>
+            </div>
+            <div class="detail_price">
+              <span>ผ่อน 0 %</span>
+            </div>
+            <div class="create-order__product-detail-01">
+              <h2>รายละเอียดสินค้า</h2>
+              <ul>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+              </ul>
+            </div>
+            <div class="order-product__price-amount">
+
+              <span><strong>จำนวนสินค้าในร้าน: </strong>
+                {{ productById.productAmount }} ชิ้น</span
+              >
+            </div>
           </div>
-          <div class="order-product__name">
-            <h1>{{ productById.productName }}</h1>
+        </div>
+      </div>
+      <div class="create-order__right">
+        <v-card class="create-order__input-amount">
+          <div>
+            <v-text-field
+              name="orderName"
+              label="Order Name"
+              id="orderName"
+              v-model="orderName"
+            ></v-text-field>
           </div>
-          <div class="order-product__price-amount">
-            <span><strong>ราคา</strong> {{ productById.productPrice }} บาท</span>
-            <span><strong>จำนวน</strong> {{ productById.productAmount }} ชิ้น</span>
+          
+          <div><p class="create-order__price-number">ราคา : ฿ {{ productById.productPrice }}</p></div>
+          <div class="create-order__title-amount"><h2>จำนวน:</h2></div>
+          <div class="create-order__set-amount">
+            <div @click="increaseAmount()" class="set-amount__add">+</div>
+            <div class="set-amount__number">{{ productAmount }}</div>
+            <div @click="decreaseAmount()" class="set-amount__remove">-</div>
+          </div>
+          <div class="create-order__create-order-btn">
+            <v-btn @click="createOrder()" color="success">ซื้อสินค้า</v-btn>
+            <v-btn @click="cancelOrder()" color="warning">ยกเลิก</v-btn>
           </div>
         </v-card>
       </div>
-    </div>
-    <div class="create-order__right">
-      <v-card class="create-order__input-amount">
-        <div>
-            <v-text-field
-                name="orderName"
-                label="Order Name"
-                id="orderName"
-                v-model="orderName"
-            ></v-text-field>
-        </div>
-        <div class="create-order__title-amount"><h1>จำนวน:</h1></div>
-        <div class="create-order__set-amount">
-          <div @click="increaseAmount()" class="set-amount__add">+</div>
-          <div class="set-amount__number">{{productAmount}}</div>
-          <div @click="decreaseAmount()" class="set-amount__remove">-</div>
-        </div>
-        <div class="create-order__create-order-btn">
-          <v-btn @click="createOrder()" color="success">ซื้อสินค้า</v-btn>
-          <v-btn @click="cancelOrder()" color="warning">ยกเลิก</v-btn>
-        </div>
-      </v-card>
     </div>
   </div>
 </template>
 
 <script>
-import {getUserToken} from "../services/AllServices";
+import NavbarComponent from "@/components/NavbarComponent.vue";
+import { getUserToken } from "../services/AllServices";
+
 export default {
+  components: {
+    NavbarComponent,
+  },
   mounted() {
     // Accessing router params in the mounted hook
     const productIdParams = this.$route.params.productId;
@@ -55,11 +80,11 @@ export default {
   },
   data() {
     return {
-      orderName:"",
+      orderName: "",
       productId: "",
       productById: {},
       productAmount: 1,
-      userToken: ""
+      userToken: "",
     };
   },
   created() {
@@ -67,7 +92,7 @@ export default {
   },
   methods: {
     cancelOrder() {
-        this.$router.push("/store");
+      this.$router.push("/store");
     },
     async getProductById() {
       try {
@@ -85,57 +110,79 @@ export default {
         console.log(error);
       }
     },
-    increaseAmount(){
-        this.productAmount++
+    increaseAmount() {
+      this.productAmount++;
     },
-    decreaseAmount(){
-        if(this.productAmount >= 2){
-            this.productAmount--
-        } else {    
-            return false;
-        }
+    decreaseAmount() {
+      if (this.productAmount >= 2) {
+        this.productAmount--;
+      } else {
+        return false;
+      }
     },
-    async createOrder(){
-        try {
-            let paramsId = this.$route.params.productId;
-            let createOrderInstance = await this.axios.post(`http://localhost:3000/orders/api/v1/products/${paramsId}/orders`, {orderName:this.orderName, orderProductAmount:this.productAmount}, {headers: {authorization: `${this.userToken}`}});
-            console.log(createOrderInstance);
-            alert("สร้าง Order สำเร็จ"+" ยอดรวม: "+ createOrderInstance.data.data.orderProductTotalPrice +" บาท");
-            
-            this.$router.push('/store');
+    async createOrder() {
+      try {
+        let paramsId = this.$route.params.productId;
+        let createOrderInstance = await this.axios.post(
+          `http://localhost:3000/orders/api/v1/products/${paramsId}/orders`,
+          { orderName: this.orderName, orderProductAmount: this.productAmount },
+          { headers: { authorization: `${this.userToken}` } }
+        );
+        console.log(createOrderInstance);
+        alert(
+          "สร้าง Order สำเร็จ" +
+            " ยอดรวม: " +
+            createOrderInstance.data.data.orderProductTotalPrice +
+            " บาท"
+        );
 
-        } catch (error) {
-            console.log(error);
-            alert("สร้าง Order ล้มเหลว");
-        }
-    }
+        this.$router.push("/store");
+      } catch (error) {
+        console.log(error);
+        alert("สร้าง Order ล้มเหลว");
+      }
+    },
   },
 };
 </script>
 
 <style scope>
-.order-product__img img{
+.create-order__price-number {
+  font-size: 30px;
+  color: red;
+  font-weight: bold;
+}
+.create-order__product-detail-01 {
+  padding: 1rem 0;
+}
+.detail_price span {
+  border: 2px solid purple;
+  padding: 0 1rem;
+}
+.order-product__img {
+  /* background: green; */
+  display: flex;
+  justify-content: center;
+}
+.order-product__img img {
   width: 230px;
   height: 230px;
   object-fit: cover;
 }
 .create-order__create-order-btn {
-    display: flex;
-    gap: 0 1rem;
+  display: flex;
+  gap: 0 1rem;
 }
 .create-order__set-amount {
+  width: 25%;
+  border: 1px solid rgb(173, 173, 173);
   
-  width: 35%;
-  box-shadow: -3px 2px 14px -6px rgba(0, 0, 0, 0.75);
-  -webkit-box-shadow: -3px 2px 14px -6px rgba(0, 0, 0, 0.75);
-  -moz-box-shadow: -3px 2px 14px -6px rgba(0, 0, 0, 0.75);
 }
 .create-order__set-amount {
   display: flex;
-  gap: 0 1rem;
+  /* gap: 0 1rem; */
   justify-content: space-between;
 }
-
 
 .create-order__input-amount {
   padding: 1rem;
@@ -144,12 +191,14 @@ export default {
 }
 .set-amount__add,
 .set-amount__remove {
-  padding: 1rem 1.5rem;
-  background: rgb(255, 247, 247);
+  padding: 0.3rem 1.5rem;
+  background: rgb(255, 255, 255);
   cursor: pointer;
+  border: 0.5px solid rgb(186, 186, 186);
 }
 .set-amount__number {
-  padding: 1rem 1.5rem;
+  padding: 0.3rem 1.5rem;
+  
 }
 .create-order-container {
   width: 100vw;
@@ -164,7 +213,7 @@ export default {
 
 .create-order__right {
   flex: 1;
-  padding: 2rem 0;
+  padding: 2rem;
   /* background: red; */
 }
 .create-order__product-detail {
